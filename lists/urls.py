d416515797f45ab0d7d89fdb8c0aad9f3a7d5b20
Hasan Alpaslan <hasan.alpaslan@gmail.com>
2014-09-30 22:44:06 +0000

    Refactored URLs

---
+++ lists/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('lists.views',
+    url(r'^(\d+)/$', 'view_list', name='view_list'),
+    url(r'^(\d+)/add_item$', 'add_item', name='add_item'),
+    url(r'^new$', 'new_list', name='new_list'),
+)
